f6868cb957fd74decd5d831175a5ba6e5e67d109
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-15 00:36:13 -0400

    Initial backend commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Small Team Management System
---
+++ backend/stms_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StmsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stms_app'
