8acbb7b0661153f8ca6b8dc54af5c762ba5757b2
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-15 18:34:02 -0400

    Created Viewsets for User, Soldier, Comment.

--- backend/stms_app/views.py
+++ backend/stms_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
 
-# Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class SoldierViewSet(viewsets.ModelViewSet):
+    serializer_class = SoldierSerializer
+    queryset = Soldier.objects.all()
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
