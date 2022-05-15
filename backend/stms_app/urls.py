440f9c95301bd42966c4febdf26317f98411f2e9
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-15 18:42:21 -0400

    Created urls for User, Soldier, Comments.

--- backend/stms_app/urls.py
+++ backend/stms_app/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+router = DefaultRouter()
+router.register("users", views.UserViewSet, basename='user')
+router.register("soldiers", views.SoldierViewSet, basename='soldier')
+router.register("comments", views.CommentViewSet, basename="comment")
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
