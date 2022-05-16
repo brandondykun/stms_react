c01bc084c35d56bc44d0c99311940e332867a1c6
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-16 11:21:22 -0400

    Added Create Account Page to App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import LoginPage from "./Pages/LoginPage";
+import CreateAccountPage from "./Pages/CreateAccountPage";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Components/Navbar";
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/create-account" element={<CreateAccountPage />} />
         </Routes>
       </BrowserRouter>
     </div>
