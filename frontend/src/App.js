4c591de73cfafb1a8f491c63706afe31871c749f
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-16 01:47:46 -0400

    Added HomePage to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import LoginPage from "./Pages/LoginPage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
