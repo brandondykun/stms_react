2e7f74e4fc019688423f9deedd21e1a7071f09ec
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-16 01:46:50 -0400

    Created Navbar Component.

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("tokens");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <Link to="/login">Log in</Link>
+      <button onClick={handleLogout}>Log out</button>
+    </div>
+  );
+};
+
+export default Navbar;
+<nav></nav>;
