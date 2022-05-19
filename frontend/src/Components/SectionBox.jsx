53f60930d3eb51f37d696f42397cf11a34572080
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-19 03:45:53 -0400

    Created SectionBox Component.

---
+++ frontend/src/Components/SectionBox.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const SectionBox = ({ section, soldiers }) => {
+  const [filteredSoldiers, setFilteredSoldiers] = useState();
+
+  useEffect(() => {
+    const filteredBySection = soldiers
+      ? soldiers.filter((soldier) => {
+          return soldier.section === section;
+        })
+      : null;
+    setFilteredSoldiers(filteredBySection);
+  }, [soldiers]);
+
+  return (
+    <div className="section-container">
+      <div className="section-title">{section}</div>
+      {filteredSoldiers &&
+        filteredSoldiers.map((soldier) => {
+          return (
+            <div className="soldier-role-name-container">
+              <div>{soldier.role}</div>
+              <Link
+                to={`/soldier-info/${soldier.id}`}
+                style={{ display: "inline-block" }}
+                className="name-link"
+              >
+                {soldier.rank} {soldier.last_name}
+              </Link>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default SectionBox;
