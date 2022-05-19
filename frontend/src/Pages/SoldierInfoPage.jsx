52825dc961deec2cbe5436325a10d5afbf5d15c7
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-19 03:49:06 -0400

    Added soldier info to SoldierInfoPage.

--- frontend/src/Pages/SoldierInfoPage.jsx
+++ frontend/src/Pages/SoldierInfoPage.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState, useSyncExternalStore } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import apiCalls from "../apiCalls/apiCalls";
 
 const SoldierInfoPage = () => {
+  const [currentSoldier, setCurrentSoldier] = useState();
   const { id } = useParams();
 
+  useEffect(() => {
+    apiCalls.getSoldierById(id).then((res) => {
+      if (res.status === 200) {
+        setCurrentSoldier(res.data);
+      }
+    });
+  }, [id]);
+
   return (
-    <div className="main-content">
+    <div className="primary-content">
       <div>Soldier {id} info page</div>
+      {currentSoldier && <div>{currentSoldier.first_name}</div>}
       <Link to={`/soldier-info/${id}/comments`}>View Comments</Link>
     </div>
   );
