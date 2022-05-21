a21368ea50f1d3334aecf063922318de568e1612
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-21 20:05:51 +0200

    Added CommentCategoryContainer for each comment category.

--- frontend/src/Pages/CommentsPage.jsx
+++ frontend/src/Pages/CommentsPage.jsx
@@ -1,13 +1,56 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AddCommentForm from "../Components/AddCommentForm";
+import apiCalls from "../apiCalls/apiCalls";
+import CommentCategoryContainer from "../Components/CommentCategoryContainer";
 
 const CommentsPage = () => {
+  const [allComments, setAllComments] = useState(null);
   const { id } = useParams();
+
+  useEffect(() => {
+    apiCalls
+      .getAllComments()
+      .then((res) => {
+        const userComments = res.data.filter((comment) => {
+          return comment.soldier === parseInt(id);
+        });
+        setAllComments(userComments);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
   return (
     <div className="primary-content">
       <div>Comments Page for {id}</div>
-      <AddCommentForm />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"CHARACTER"}
+      />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"PRESENCE"}
+      />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"INTELLECT"}
+      />
+      <CommentCategoryContainer allComments={allComments} category={"LEADS"} />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"DEVELOPS"}
+      />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"ACHIEVES"}
+      />
+      <CommentCategoryContainer
+        allComments={allComments}
+        category={"OVERALL"}
+      />
+      <AddCommentForm allComments={allComments} />
     </div>
   );
 };
