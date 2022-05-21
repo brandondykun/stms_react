53d693c3dbea8f68d7d57b509b1e5c240c85a9be
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-21 20:04:21 +0200

    Created ComentCategoryContainer component.

---
+++ frontend/src/Components/CommentCategoryContainer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const CommentCategoryContainer = ({ allComments, category }) => {
+  const [commentsFilteredByCategory, setCommentsFilteredByCategory] =
+    useState(null);
+
+  useEffect(() => {
+    const filteredComments = allComments
+      ? allComments.filter((comment) => {
+          return comment.category === category;
+        })
+      : null;
+    setCommentsFilteredByCategory(filteredComments);
+  }, [allComments]);
+
+  return (
+    <div className="comments-container">
+      <div className="comments-container-title">{category}</div>
+      <div className="soldier-details-info-container">
+        {commentsFilteredByCategory &&
+          commentsFilteredByCategory.map((comment) => {
+            return <div key={comment.id}>-{comment.comment_text}</div>;
+          })}
+        {commentsFilteredByCategory &&
+          commentsFilteredByCategory.length === 0 && <div>No Comments</div>}
+      </div>
+    </div>
+  );
+};
+
+export default CommentCategoryContainer;
