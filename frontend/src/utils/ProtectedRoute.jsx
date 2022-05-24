e81779f730a4087168ca4830d2fc28d1e38aaa83
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-24 15:06:22 +0100

    Added Loading return to prevent immediate redirect back to login page.

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,6 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ userId, redirectPath = "/login", children }) => {
+const ProtectedRoute = ({
+  userId,
+  redirectPath = "/login",
+  children,
+  isLoading,
+}) => {
+  if (isLoading) {
+    return (
+      <div className="primary-content">
+        <h2>LOADING...</h2>
+      </div>
+    );
+  }
   if (!userId) {
     return <Navigate to={redirectPath} replace />;
   }
