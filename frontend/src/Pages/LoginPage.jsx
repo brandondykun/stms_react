7c5e01a0f45d94172e1355a83102872753119395
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-24 10:33:15 +0100

    Changed login handler function to async await, and also added setUser functionality to set state with full user data.

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
-const LoginPage = ({ setUserId }) => {
+const LoginPage = ({ setUserId, setUser }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState(null);
@@ -15,20 +15,41 @@ const LoginPage = ({ setUserId }) => {
 
     const data = { username: username, password: password };
     // console.log("DATA: ", data);
-    apiCalls
-      .login(data)
-      .then((res) => {
-        if (res.status === 200) {
-          const decodedToken = jwt_decode(res.data.access);
-          setUserId(decodedToken);
-          localStorage.setItem("tokens", JSON.stringify(res.data));
-          navigate("/home");
-        }
-      })
-      .catch((err) => {
-        setErrors(err);
-        console.log(err);
-      });
+    // apiCalls
+    //   .login(data)
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       const decodedToken = jwt_decode(res.data.access);
+    //       setUserId(decodedToken);
+    //       localStorage.setItem("tokens", JSON.stringify(res.data));
+    //       // navigate("/home");
+    //       return decodedToken.user_id;
+    //     }
+    //   })
+    //   .then((res) => {
+    //     apiCalls.getUserById(res)
+    //     // navigate("/home");
+    //   })
+    //   .catch((err) => {
+    //     setErrors(err);
+    //     console.log(err);
+    //   });
+
+    const setLoginUserData = async () => {
+      const loginRes = await apiCalls.login(data);
+      if (loginRes.status === 200) {
+        const decodedToken = jwt_decode(loginRes.data.access);
+        setUserId(decodedToken);
+        localStorage.setItem("tokens", JSON.stringify(loginRes.data));
+        const user = await apiCalls.getUserById(decodedToken.user_id);
+        const soldier_id = user.data.soldier;
+        const soldier = await apiCalls.getSoldierById(soldier_id);
+        setUser(soldier);
+        navigate("/home");
+      }
+    };
+
+    setLoginUserData();
   };
 
   return (
