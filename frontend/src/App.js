72db1bca1b71220ba33d5aa5c43188a50aa613f1
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-24 15:07:59 +0100

    Added UseEffect to update user state and prevent redirect to login on page refresh.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,38 @@ import MyInfoPage from "./Pages/MyInfoPage";
 import AdminPage from "./Pages/AdminPage";
 import SoldierInfoPage from "./Pages/SoldierInfoPage";
 import CommentsPage from "./Pages/CommentsPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./Components/Footer";
 import ProtectedRoute from "./utils/ProtectedRoute";
+import jwt_decode from "jwt-decode";
+import apiCalls from "./apiCalls/apiCalls";
 
 function App() {
   const [userId, setUserId] = useState();
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkIfUserLoggedIn = async () => {
+      const tokens = JSON.parse(localStorage.getItem("tokens"));
+      if (tokens) {
+        const accessToken = tokens.access;
+        const decodedToken = jwt_decode(accessToken);
+        setUserId(decodedToken.user_id);
+        const user = await apiCalls.getUserById(decodedToken.user_id);
+        if (user.status === 200) {
+          const soldier_id = user.data.soldier;
+
+          const soldier = await apiCalls.getSoldierById(soldier_id);
+          if (soldier.status === 200) {
+            setUser(soldier.data);
+          }
+        }
+      }
+      setIsLoading(false);
+    };
+    checkIfUserLoggedIn();
+  }, []);
 
   return (
     <div className="App">
@@ -27,7 +52,9 @@ function App() {
             path="/login"
             element={<LoginPage setUserId={setUserId} setUser={setUser} />}
           />
-          <Route element={<ProtectedRoute userId={userId} />}>
+          <Route
+            element={<ProtectedRoute userId={userId} isLoading={isLoading} />}
+          >
             <Route path="/home" element={<HomePage />} />
             <Route
               path="/my-info"
