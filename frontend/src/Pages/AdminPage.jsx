ed7f77733d2463ccaa822bef9b7010c3b1cf6541
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-25 10:31:14 +0100

    Added useEffect and AdminSectionBox components.

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import apiCalls from "../apiCalls/apiCalls";
+import AdminSectionBox from "../Components/AdminSectionBox";
+
 const AdminPage = () => {
+  const [allSoldiers, setAllSoldiers] = useState();
+
+  useEffect(() => {
+    apiCalls
+      .getAllSoldiers()
+      .then((res) => {
+        if (res.status === 200) {
+          setAllSoldiers(res.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="primary-content">
       <div>This is the Admin Page</div>
+      <AdminSectionBox section="BN STAFF" soldiers={allSoldiers} />
+      <AdminSectionBox section="ALPHA" soldiers={allSoldiers} />
+      <AdminSectionBox section="BRAVO" soldiers={allSoldiers} />
+      <AdminSectionBox section="CHARLIE" soldiers={allSoldiers} />
+      <AdminSectionBox section="UNASSIGNED" soldiers={allSoldiers} />
     </div>
   );
 };
