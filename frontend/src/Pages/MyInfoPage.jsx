b5d6e7ece5395a77d8daf8371256094e00c06b0b
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-26 00:23:29 +0200

    Added setCurrentSoldier prop to SoldierDetails component.

--- frontend/src/Pages/MyInfoPage.jsx
+++ frontend/src/Pages/MyInfoPage.jsx
@@ -3,7 +3,7 @@ import SoldierDetails from "../Components/SoldierDetails";
 const MyInfoPage = ({ user, setUser }) => {
   return (
     <div className="primary-content">
-      <SoldierDetails currentSoldier={user} />
+      <SoldierDetails currentSoldier={user} setCurrentSoldier={setUser} />
     </div>
   );
 };
