f639e2dc58359da4b08903af64df03499880d926
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-27 16:10:23 +0200

    Added link to comments page from MyInfoPage.

--- frontend/src/Pages/MyInfoPage.jsx
+++ frontend/src/Pages/MyInfoPage.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
 import SoldierDetails from "../Components/SoldierDetails";
 
 const MyInfoPage = ({ loggedInSoldier, setLoggedInSoldier }) => {
   return (
     <div className="primary-content">
+      <h1>My Info</h1>
+      <Link
+        className="text-link"
+        to={`/soldier-info/${loggedInSoldier.id}/comments`}
+      >
+        View My Performance Comments >>
+      </Link>
       <SoldierDetails
         currentSoldier={loggedInSoldier}
         setCurrentSoldier={setLoggedInSoldier}
