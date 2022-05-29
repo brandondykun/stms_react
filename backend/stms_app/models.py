b1a44f45b12f2d2f040d61369123ec641c849533
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-28 22:35:31 -0400

    Changed user field in Soldier model to primary_key=True.

--- backend/stms_app/models.py
+++ backend/stms_app/models.py
@@ -8,7 +8,7 @@ from stms_app.utils import assign_unit_position
 class Soldier(models.Model):
     """Detailed Information about each soldier."""
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="soldier")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=30)
     middle_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
