1b7162e688148252374e7a1828d6fc4148c19ca6
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-30 12:01:58 -0400

    Added user field to SoldierSerializer.

--- backend/stms_app/serializers.py
+++ backend/stms_app/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", 'password', "soldier"]
 
     password = serializers.CharField(write_only=True)
-    soldier = serializers.IntegerField(read_only=True)
+    # soldier = serializers.IntegerField(read_only=True)
 
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data["password"]) # hashes our password
@@ -21,7 +21,7 @@ class SoldierSerializer(serializers.ModelSerializer):
     # user= UserSerializer(read_only=True)
     class Meta:
         model = Soldier
-        fields = ["id", "first_name", "middle_name", "last_name", "rank", "grade", "pebd", "date_of_rank", "expiration_term_of_service", "section", "team", "role", "acft_score", "m4_qual", "dlc_1_complete", "blc_complete", "dlc_2_complete", "alc_complete", "dlc_3_complete", "slc_complete", "jfo_qualified", "drivers_license", "is_leader", "unit_position"]
+        fields = ["id", "user", "first_name", "middle_name", "last_name", "rank", "grade", "pebd", "date_of_rank", "expiration_term_of_service", "section", "team", "role", "acft_score", "m4_qual", "dlc_1_complete", "blc_complete", "dlc_2_complete", "alc_complete", "dlc_3_complete", "slc_complete", "jfo_qualified", "drivers_license", "is_leader", "unit_position"]
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
