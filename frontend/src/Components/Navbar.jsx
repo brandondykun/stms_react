faa775a0f8fce2708d97dc32efb0f2ed4d664961
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-31 14:27:12 -0400

    Converted Link's to NavLinks.

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ setLoggedInSoldier, loggedInSoldier }) => {
@@ -13,38 +13,38 @@ const Navbar = ({ setLoggedInSoldier, loggedInSoldier }) => {
   return (
     <div className="primary-nav">
       {!loggedInSoldier && (
-        <Link className="navbar-link" to="/login">
+        <NavLink className="navbar-link" to="/login">
           <span className="nav-link-text">Log In</span>{" "}
           <i class="fa-solid fa-right-to-bracket"></i>
-        </Link>
+        </NavLink>
       )}
 
       {!loggedInSoldier && (
-        <Link className="navbar-link" to="/register">
+        <NavLink className="navbar-link" to="/register">
           <span className="nav-link-text">Register</span>{" "}
           <i class="fa-solid fa-user-plus"></i>
-        </Link>
+        </NavLink>
       )}
 
       {loggedInSoldier && loggedInSoldier.first_name && (
-        <Link className="navbar-link" to="/home">
+        <NavLink className="navbar-link" to="/home">
           <span className="nav-link-text">Home</span>{" "}
           <i class="fa-solid fa-house"></i>
-        </Link>
+        </NavLink>
       )}
 
       {loggedInSoldier && loggedInSoldier.first_name && (
-        <Link className="navbar-link" to="/my-info">
+        <NavLink className="navbar-link" to="/my-info">
           <span className="nav-link-text">My Info</span>{" "}
           <i class="fa-solid fa-user"></i>
-        </Link>
+        </NavLink>
       )}
 
       {loggedInSoldier && loggedInSoldier.is_staff && (
-        <Link className="navbar-link" to="/admin">
+        <NavLink className="navbar-link" to="/admin">
           <span className="nav-link-text">Admin</span>{" "}
           <i class="fa-solid fa-list-check"></i>
-        </Link>
+        </NavLink>
       )}
 
       {loggedInSoldier && loggedInSoldier.first_name && (
