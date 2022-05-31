5aa7fa5b2fa14b65ff6c1aed5f650660243a3c20
Brandon Dykun <brandon.dykun@gmail.com>
2022-05-31 11:21:55 -0400

    Removed unneeded api call in form submit (reflects changes in new database design).

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -12,31 +12,20 @@ const LoginPage = ({ setUserId, setLoggedInSoldier, setIsLoading }) => {
 
   const handleLoginFormSubmit = (e) => {
     e.preventDefault();
-    console.log("Trying to log in");
     const data = { username: username, password: password };
 
     const setLoginUserData = async () => {
       const loginRes = await apiCalls.login(data);
       if (loginRes.status === 200) {
-        console.log("LOGIN RES: ", loginRes);
         const decodedToken = jwt_decode(loginRes.data.access);
-        console.log("DECODED TOKEN: ", decodedToken.user_id);
         setUserId(decodedToken.user_id);
         localStorage.setItem("tokens", JSON.stringify(loginRes.data));
 
-        console.log("TRYING TO GET USER");
-        const user = await apiCalls.getUserById(decodedToken.user_id);
-        console.log("LOGIN USER: ", user);
+        const user = await apiCalls.getSoldierById(decodedToken.user_id);
         if (user.status === 200) {
-          const soldier_id = user.data.soldier;
-
-          const soldier = await apiCalls.getSoldierById(soldier_id);
-          if (soldier.status === 200) {
-            console.log("SOLDIER DATA: ", soldier.data);
-            setLoggedInSoldier(soldier.data);
-            setIsLoading(false);
-            navigate("/home");
-          }
+          setLoggedInSoldier(user.data);
+          setIsLoading(false);
+          navigate("/home");
         }
       }
     };
@@ -55,7 +44,6 @@ const LoginPage = ({ setUserId, setLoggedInSoldier, setIsLoading }) => {
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            // placeholder="Username"
             aria-label="Username"
           />
           <label htmlFor="password">Password</label>
@@ -64,7 +52,6 @@ const LoginPage = ({ setUserId, setLoggedInSoldier, setIsLoading }) => {
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            // placeholder="Password"
             aria-label="Password"
           />
           <button className="login-register-button" type="submit">
