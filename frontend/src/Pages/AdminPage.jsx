1f574081563d92d0037011e1725de2e515bd9ad8
Brandon Dykun <brandon.dykun@gmail.com>
2022-06-01 15:08:19 -0400

    Added SeparateSoon components and wrapped AdminSectionBox components and SeparateSoon components in div wrappers to display them using flex styling on larger screens.

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import apiCalls from "../apiCalls/apiCalls";
 import AdminSectionBox from "../Components/AdminSectionBox";
+import SeparateSoon from "../Components/SeparateSoon";
 
 const AdminPage = ({ loggedInSoldier, setLoggedInSoldier }) => {
   const [allSoldiers, setAllSoldiers] = useState();
@@ -20,51 +21,80 @@ const AdminPage = ({ loggedInSoldier, setLoggedInSoldier }) => {
   return (
     <div className="primary-content">
       <h1 className="page-title">Admin Page</h1>
-      <AdminSectionBox
-        section="BN STAFF"
-        allSoldiers={allSoldiers}
-        setAllSoldiers={setAllSoldiers}
-        soldierToReassign={soldierToReassign}
-        setSoldierToReassign={setSoldierToReassign}
-        loggedInSoldier={loggedInSoldier}
-        setLoggedInSoldier={setLoggedInSoldier}
-      />
-      <AdminSectionBox
-        section="ALPHA"
-        allSoldiers={allSoldiers}
-        setAllSoldiers={setAllSoldiers}
-        soldierToReassign={soldierToReassign}
-        setSoldierToReassign={setSoldierToReassign}
-        loggedInSoldier={loggedInSoldier}
-        setLoggedInSoldier={setLoggedInSoldier}
-      />
-      <AdminSectionBox
-        section="BRAVO"
-        allSoldiers={allSoldiers}
-        setAllSoldiers={setAllSoldiers}
-        soldierToReassign={soldierToReassign}
-        setSoldierToReassign={setSoldierToReassign}
-        loggedInSoldier={loggedInSoldier}
-        setLoggedInSoldier={setLoggedInSoldier}
-      />
-      <AdminSectionBox
-        section="CHARLIE"
-        allSoldiers={allSoldiers}
-        setAllSoldiers={setAllSoldiers}
-        soldierToReassign={soldierToReassign}
-        setSoldierToReassign={setSoldierToReassign}
-        loggedInSoldier={loggedInSoldier}
-        setLoggedInSoldier={setLoggedInSoldier}
-      />
-      <AdminSectionBox
-        section="UNASSIGNED"
-        allSoldiers={allSoldiers}
-        setAllSoldiers={setAllSoldiers}
-        soldierToReassign={soldierToReassign}
-        setSoldierToReassign={setSoldierToReassign}
-        loggedInSoldier={loggedInSoldier}
-        setLoggedInSoldier={setLoggedInSoldier}
-      />
+      <div className="admin-page-container">
+        <div className="admin-section-boxes-container">
+          <AdminSectionBox
+            section="BN STAFF"
+            allSoldiers={allSoldiers}
+            setAllSoldiers={setAllSoldiers}
+            soldierToReassign={soldierToReassign}
+            setSoldierToReassign={setSoldierToReassign}
+            loggedInSoldier={loggedInSoldier}
+            setLoggedInSoldier={setLoggedInSoldier}
+          />
+          <AdminSectionBox
+            section="ALPHA"
+            allSoldiers={allSoldiers}
+            setAllSoldiers={setAllSoldiers}
+            soldierToReassign={soldierToReassign}
+            setSoldierToReassign={setSoldierToReassign}
+            loggedInSoldier={loggedInSoldier}
+            setLoggedInSoldier={setLoggedInSoldier}
+          />
+          <AdminSectionBox
+            section="BRAVO"
+            allSoldiers={allSoldiers}
+            setAllSoldiers={setAllSoldiers}
+            soldierToReassign={soldierToReassign}
+            setSoldierToReassign={setSoldierToReassign}
+            loggedInSoldier={loggedInSoldier}
+            setLoggedInSoldier={setLoggedInSoldier}
+          />
+          <AdminSectionBox
+            section="CHARLIE"
+            allSoldiers={allSoldiers}
+            setAllSoldiers={setAllSoldiers}
+            soldierToReassign={soldierToReassign}
+            setSoldierToReassign={setSoldierToReassign}
+            loggedInSoldier={loggedInSoldier}
+            setLoggedInSoldier={setLoggedInSoldier}
+          />
+          <AdminSectionBox
+            section="UNASSIGNED"
+            allSoldiers={allSoldiers}
+            setAllSoldiers={setAllSoldiers}
+            soldierToReassign={soldierToReassign}
+            setSoldierToReassign={setSoldierToReassign}
+            loggedInSoldier={loggedInSoldier}
+            setLoggedInSoldier={setLoggedInSoldier}
+          />
+        </div>
+
+        <div className="separate-soon-container">
+          <h2>Upcomming ETS</h2>
+          <SeparateSoon allSoldiers={allSoldiers} daysStart={0} daysStop={30} />
+          <SeparateSoon
+            allSoldiers={allSoldiers}
+            daysStart={30}
+            daysStop={60}
+          />
+          <SeparateSoon
+            allSoldiers={allSoldiers}
+            daysStart={60}
+            daysStop={90}
+          />
+          <SeparateSoon
+            allSoldiers={allSoldiers}
+            daysStart={90}
+            daysStop={180}
+          />
+          <SeparateSoon
+            allSoldiers={allSoldiers}
+            daysStart={180}
+            daysStop={365}
+          />
+        </div>
+      </div>
     </div>
   );
 };
